730d187323a79d48552efdcac47775183496e3af
Ridho Prawira Bahari <ridhoprawira.crb123@gmail.com>
2020-12-29 14:31:32 +0700

    initial commit
    initial commit

---
+++ Assets/Script/MyEditorScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public class MyEditorScript : MonoBehaviour
+{
+    
+    /*static void PerformBuild ()
+     {
+        string[] defaultScenes = { "Assets/Scenes/SampleScene.unity" };
+         BuildPipeline.BuildPlayer(defaultScenes,"./builds/",
+         BuildTarget.Android);
+     }*/
+
+    [MenuItem("Build/Build Android")]
+    public static void PerformBuild()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/SampleScene.unity"};
+        buildPlayerOptions.locationPathName = "AndroidBuild";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+
+     
+}
